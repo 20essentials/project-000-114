1297109b88a369f4c880a482a54d8bd3c1e247f7
20essentials <s0l0estudi4r@gmail.com>
2024-10-04 13:21:07 +0200

    v0.0.1

---
+++ script.js
@@ -0,0 +1,13 @@
+console.groupCollapsed("REFERENCE ⬇️")
+console.log(
+  'Images',
+  'https://wallhaven.cc/w/jx1gxy',
+  'https://wallhaven.cc/w/x67prv',
+  'https://wallhaven.cc/w/6q9lr7',
+  'https://wallhaven.cc/w/85k2lj',
+  'https://wallhaven.cc/w/3lwxjv'
+);
+console.log(
+  'Online Tutorials',
+  'https://youtu.be/_CmH5CL6OHU?si=in1EDHOy3zypOws_'
+);
